eba80b6da45346d33e774a2ab56d035a0cfae28e
Anna Petry <annajoellepetry@gmail.com>
2014-10-09 15:44:16 -0700

    delete smpt file

--- config/initializers/smpt_settings.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-    :enable_starttls_auto => true,
-    :address        => 'smtp.gmail.com',
-    :port           => 587,
-    :domain         => 'petryportfolio.herokuapp.com',
-    :authentication => :plain,
-    :user_name      => 'hello@annapetry.com',
-    :password       => 'firecracker0207'
-  }
