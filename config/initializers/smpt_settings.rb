8a3202034d79b147a7aa6cf54d7ff6a2359e875a
Anna Petry <annajoellepetry@gmail.com>
2014-10-09 15:22:12 -0700

    Hide smpt settings

--- config/initializers/smpt_settings.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-    :enable_starttls_auto => true,
-    :address        => 'smtp.gmail.com',
-    :port           => 587,
-    :domain         => 'www.annapetry.com',
-    :authentication => :plain,
-    :user_name      => 'hello@annapetry.com',
-    :password       => 'firecracker0207'
-  }
